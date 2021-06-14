b33e1fb1d4edbb79e533e5339ab9685cbcddcf93
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-06-14 14:14:11 +0900

    Fix to rest proxy.

--- backend/apps/apiboss/apis/restproxy.js
+++ backend/apps/apiboss/apis/restproxy.js
@@ -10,12 +10,12 @@ exports.doService = doService;
 async function doService(req) {
     const method = req.method.toLowerCase(); const url = new URL(req.url); const host = url.hostname; 
     const port = url.port; const path = url.pathname+url.search; if (!path.startsWith("/")) path = `/${path}`;
-    const headers = {...req.headers}; const data = req.data;
+    const headers = {...req.headers}; const reqObj = req.data;
 
     if (url.protocol.toLowerCase() == "https:") method += "Https";           
     if (method == "delete") method = "deleteHttp";        // delete is a reserved word in JS
 
-    const {error,data,status,resHeaders} = await rest[method](host, port, path, headers, data);
+    const {error,data,status,resHeaders} = await rest[method](host, port, path, headers, reqObj);
     if (error) throw("APIBoss Proxy Error", {status, message:error}); 
     else return({data,headers: resHeaders});
 }
