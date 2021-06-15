4dc22a749f08a07d69dafd9362c7d4f59d32ce8d
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-06-15 12:56:29 +0900

    Minor coding style cleanup.

--- backend/apps/apiboss/lib/apiregistry_extensions/apibossheaderinjector.js
+++ backend/apps/apiboss/lib/apiregistry_extensions/apibossheaderinjector.js
@@ -1,15 +1,14 @@
-/* 
+/**
  * (C) 2020 TekMonks. All rights reserved.
  * License: See enclosed LICENSE file.
  * 
  * Injects custom headers from APIBoss
  */
-const jwttokenmanager = require(`${CONSTANTS.LIBDIR}/apiregistry_extensions/jwttokenmanager.js`);
 
 function injectResponseHeaders(apiregentry, url, response, requestHeaders, responseHeaders) {
     const headersToAdd = response?.headers;
     if (headersToAdd) for (const headerName of Object.keys(headersToAdd)) responseHeaders[headerName] = headersToAdd[headerName];
-    if (response.data.result) jwttokenmanager.injectResponseHeadersInternal(apiregentry, url, response, requestHeaders, responseHeaders);
+    if (response.data.result) APIREGISTRY.getExtension("jwttokenmanager").injectResponseHeadersInternal(apiregentry, url, response, requestHeaders, responseHeaders);
 }
 
 module.exports = {injectResponseHeaders};
